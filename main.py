219eb0d689a97fc5e8b1e0ce0f657f6f16662a13
gosa-pixel <hrdluis4@gmail.com>
2026-09-20 20:40:55 +0200

    make tests a python package

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main():
     )
 
     parser.add_argument(
-        "augmentation",
+        "--augmentation",
         required=True,
         choices=[
             "none",
@@ -22,19 +22,19 @@ def main():
     )
 
     parser.add_argument(
-        "pretrain-epochs",
+        "--pretrain-epochs",
         type=int,
         default=10
     )
 
     parser.add_argument(
-        "evaluation-epochs",
+        "--evaluation-epochs",
         type=int,
         default=5
     )
 
     parser.add_argument(
-        "batch-size",
+        "--batch-size",
         type=int,
         default=128
     )
